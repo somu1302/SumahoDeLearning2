5b5e25136c8821c0e1b71ae77c7f2243fc99edb4
somu1302 <glacia00471@gmail.com>
2024-06-02 17:13:44 +0900

    v24.06.02.1

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "somu1302-スマホでラーニング-24.06.02.1";
-const contentToCache = [
-    "Build/SumahoDeLearning(Web).loader.js",
-    "Build/SumahoDeLearning(Web).framework.js",
-    "Build/SumahoDeLearning(Web).data",
-    "Build/SumahoDeLearning(Web).wasm",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
